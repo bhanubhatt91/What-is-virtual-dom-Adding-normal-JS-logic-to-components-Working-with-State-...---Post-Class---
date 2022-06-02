e9031042580ed47906fc3f23935e8973878bb9d8
bhanubhatt91 <103954506+bhanubhatt91@users.noreply.github.com>
2022-06-02 11:14:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@
 // );
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById('root'));
