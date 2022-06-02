a136c0d7a67c87b4b51a19d54eca991c63e25257
bhanubhatt91 <103954506+bhanubhatt91@users.noreply.github.com>
2022-06-02 11:18:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-// import React from "react";
-// import ReactDOM from 'react-dom';
-// import App from "./components/App";
-
-// const rootElement = document.getElementById("root");
-// const root = ReactDOM.createRoot(rootElement);
-
-// root.render(
-//     <App />
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
